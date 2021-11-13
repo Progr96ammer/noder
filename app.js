316f7a11a64706b2c4161f98b10bba84e3b067ff
Progr96ammer <69406656+Progr96ammer@users.noreply.github.com>
2021-11-13 22:20:01 +0300

    xfhxth

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.SECRET_KEY));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('./public'));
 app.use('/', indexRouter);
 app.use('/user', usersRouter);
 app.use(function(req, res, next) {
